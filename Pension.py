d2093138b31e7b8a800df480b89d29f445c71ac6
Kevin Chacón <13-10268@usb.ve>
2020-01-29 09:53:56 -0400

    Verificacion de validez de la entrada

--- Pension.py
+++ Pension.py
@@ -4,4 +4,7 @@ class VerificaccionPension():
     today = datetime.datetime.now()
     def __init__(self,d = datetime.datetime.now() ):
         self.dia, self.mes, self.año = [int(x) for x in d.split('/')]
-       
+        try:
+            self.inicio = datetime.datetime(self.año, self.mes, self.dia)
+        except:
+            print("Fecha no valida")
