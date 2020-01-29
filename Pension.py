e0a759608676ec5a29c6add0a66276fb46799fe6
Kevin Chacón <13-10268@usb.ve>
2020-01-29 10:05:05 -0400

    Agregada condicion de años de trabajo bajo condiciones especiales (Art. 28)

--- Pension.py
+++ Pension.py
@@ -2,9 +2,13 @@ import datetime
 
 class VerificaccionPension():
     today = datetime.datetime.now()
-    def __init__(self,d = datetime.datetime.now(), s= 'M'):
+    def __init__(self,d = datetime.datetime.now(), s= 'M', i=0):
         self.dia, self.mes, self.año = [int(x) for x in d.split('/')]
         self.sexo = s
+        try:
+            self.insalubres = i//4
+        except:
+            print("Numero de años bajo condiciones insalubres invalido")
         try:
             self.inicio = datetime.datetime(self.año, self.mes, self.dia)
         except:
