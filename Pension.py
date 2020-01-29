fab6ee8bef601ae88ab4f97ff3f78b41a0726604
Kevin Chacón <13-10268@usb.ve>
2020-01-29 09:59:58 -0400

    Agregada condicion de sexo (Art. 27)

--- Pension.py
+++ Pension.py
@@ -2,16 +2,18 @@ import datetime
 
 class VerificaccionPension():
     today = datetime.datetime.now()
-    def __init__(self,d = datetime.datetime.now() ):
+    def __init__(self,d = datetime.datetime.now(), s= 'M'):
         self.dia, self.mes, self.año = [int(x) for x in d.split('/')]
+        self.sexo = s
         try:
             self.inicio = datetime.datetime(self.año, self.mes, self.dia)
         except:
             print("Fecha no valida")
         assert (self.inicio < self.today)
-        
     def _EsPensionado(self):
-        if ((self.today - self.inicio).days) >= 365*(60):
+       if(self.sexo == 'M')and( (self.today - self.inicio).days) >= 365:
             print("Es pensionado")
+        elif(self.sexo == 'F')and( (self.today - self.inicio).days) >= 365:
+            print("Es pensionada")
         else:
             print("No es pensionado")
